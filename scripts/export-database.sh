47ba4580c67f38d56989152932f22a32fb1962b1
greg <greg@vixns.com>
2021-05-17 17:19:36 +0200

    Revert "docker compose is now in the docker cli"
    This reverts commit f6432194461591900d9583c8fc972a1f9496ab27.

--- scripts/export-database.sh
+++ scripts/export-database.sh
@@ -5,5 +5,5 @@
 ROOT_DIR="$(dirname $(realpath $0))/../"
 CUR_DIR=$(pwd)
 cd $ROOT_DIR
-docker compose exec -T db sh -c "mysqldump -uroot -p\${MYSQL_ROOT_PASSWORD} --max_allowed_packet=512M --default-character-set=utf8mb4 --single-transaction --routines --complete-insert --add-drop-table --quick --quote-names \${MYSQL_DATABASE}" | gzip -9 > export.sql.gz
+docker-compose exec db sh -c "mysqldump -uroot -p\${MYSQL_ROOT_PASSWORD} --max_allowed_packet=512M --default-character-set=utf8mb4 --single-transaction --routines --complete-insert --add-drop-table --quick --quote-names \${MYSQL_DATABASE}" | gzip -9 > export.sql.gz
 cd $CUR_DIR
