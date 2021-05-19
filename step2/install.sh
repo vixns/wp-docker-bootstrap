236aed66661e0ffe593ff0abba3675243d2081a7
greg <greg@vixns.com>
2021-05-19 08:44:25 +0200

    fix typo

--- step2/install.sh
+++ step2/install.sh
@@ -50,8 +50,8 @@ then
 else
     cat > cleanup.sh << EOF
 #!/bin/sh
-[ -e docker compose.yml ] && docker-compose down -v
-rm -rf .mc s3 wordpress docker compose.yml .vixns-ci.yml Jenkinsfile
+[ -e docker-compose.yml ] && docker-compose down -v
+rm -rf .mc s3 wordpress docker-compose.yml .vixns-ci.yml Jenkinsfile
 EOF
   chmod +x cleanup.sh
 fi
