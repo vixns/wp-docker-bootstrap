043bb2a2315a31796541c1eaed1eb24fbda7c1a1
greg <greg@vixns.com>
2021-06-09 16:53:17 +0200

    fix search/replace

--- setup.sh
+++ setup.sh
@@ -412,7 +412,8 @@ then
   wait 3
 fi
 wp core update-db
-wp search-replace --recurse-objects --all-tables \$(wp option get siteurl) \${WP_URL}
+OLDURL=\$(echo 'SELECT option_value FROM wp_options WHERE option_name="siteurl"' | wp db query --skip-column-names)
+wp search-replace --recurse-objects --all-tables \$OLDURL \${WP_URL}
 EOF
 chmod 755 update.sh
 
